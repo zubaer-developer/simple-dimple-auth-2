06c105d10d08004f9ed989bafa9ef56219da8229
zubaer.developer <zubaer.developer@gmail.com>
2025-10-28 23:36:06 +0600

    setup signout logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import "./App.css";
 import { auth } from "./firebase/firebase";
 
@@ -19,11 +19,23 @@ function App() {
   const handleGitHubSignIn = () => {
     console.log("clicked git hub sign in");
   };
+
+  const handleSignOut = () => {
+    console.log("clicked sign out button");
+    signOut(auth)
+      .then(() => {
+        console.log("sign out successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <h1>Please Sign In</h1>
       <button onClick={handleGoogleSignIn}>Sign In With Google</button>
       <button onClick={handleGitHubSignIn}>Sign In With GitHub</button>
+      <button onClick={handleSignOut}>Sign Out</button>
     </>
   );
 }
