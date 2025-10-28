385192c88c1635354da783aa66efb74a76857ef1
zubaer.developer <zubaer.developer@gmail.com>
2025-10-28 23:45:40 +0600

    after login display name, email, photo in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import "./App.css";
 import { auth } from "./firebase/firebase";
+import { useState } from "react";
 
 const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState(null);
+
   const handleGoogleSignIn = () => {
     console.log("clicked google sign in");
     signInWithPopup(auth, googleProvider)
       .then((result) => {
         console.log(result);
+        console.log(result.user);
+        console.log(result.user.displayName);
+        console.log(result.user.email);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log(error);
@@ -36,6 +43,14 @@ function App() {
       <button onClick={handleGoogleSignIn}>Sign In With Google</button>
       <button onClick={handleGitHubSignIn}>Sign In With GitHub</button>
       <button onClick={handleSignOut}>Sign Out</button>
+
+      {user && (
+        <div>
+          <h3> {user.displayName} </h3>
+          <h3> {user.email} </h3>
+          <img src={user.photoUrl} alt="" />
+        </div>
+      )}
     </>
   );
 }
